78530184c0b55bd6d5adf5ba54bd8a298534870e
Leah <Leahzhang1595995@gmail.com>
2026-07-11 23:58:56 -0400

    home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      resolve: {
+          alias: {
+              '@': path.resolve(__dirname, 'src'),
+          },
+      },
+	},
+
+  integrations: [icon()],
+});
